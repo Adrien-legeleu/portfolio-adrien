7bc08ec5bb067040cddc80d868a669bdb3364d94
Test <adrienlegeleu@gmail.com>
2024-12-27 12:41:38 +0100

    page contact almost finished

--- app/component/Contact/Contact.tsx
+++ app/component/Contact/Contact.tsx
@@ -1,8 +1,14 @@
 "use client";
-import { BackgroundGradient } from "@/components/ui/background-gradient";
 import ScriptCopyBtn from "@/components/ui/script-copy-btn";
 import { cn } from "@/lib/utils";
 import { motion } from "framer-motion";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
+import { FormContact } from "./Form";
 
 const Contact = () => {
   const customCommandMap1 = {
@@ -20,8 +26,9 @@ const Contact = () => {
     },
   };
   return (
-    <div className="my-20 grid grid-cols-2 gap-20">
-      <div>
+    <div className="my-20 grid grid-cols-2 gap-20 px-5">
+      <FormContact />
+      <div className="space-y-10">
         <motion.div
           initial="initial"
           animate="animate"
@@ -58,8 +65,34 @@ const Contact = () => {
             commandMap={customCommandMap2}
           />
         </motion.div>
+        <div className="space-y-5">
+          <h4 className="text-xl text-center">
+            Vous vous posez des questions ?
+          </h4>
+          <Accordion type="single" collapsible className="w-full">
+            <AccordionItem value="item-1">
+              <AccordionTrigger>Is it accessible?</AccordionTrigger>
+              <AccordionContent>
+                Yes. It adheres to the WAI-ARIA design pattern.
+              </AccordionContent>
+            </AccordionItem>
+            <AccordionItem value="item-2">
+              <AccordionTrigger>Is it styled?</AccordionTrigger>
+              <AccordionContent>
+                Yes. It comes with default styles that matches the other
+                components&apos; aesthetic.
+              </AccordionContent>
+            </AccordionItem>
+            <AccordionItem value="item-3">
+              <AccordionTrigger>Is it animated?</AccordionTrigger>
+              <AccordionContent>
+                Yes. It's animated by default, but you can disable it if you
+                prefer.
+              </AccordionContent>
+            </AccordionItem>
+          </Accordion>
+        </div>
       </div>
-      <div>FAQ</div>
     </div>
   );
 };
---
+++ app/component/Contact/Form.tsx
@@ -0,0 +1,83 @@
+"use client";
+import React from "react";
+import { cn } from "@/lib/utils";
+import { IconBrandGithub } from "@tabler/icons-react";
+import { Label } from "./LabelContact";
+import { Input } from "./inputContact";
+import { Textarea } from "./TextareaContact";
+import { IconSend } from "@tabler/icons-react";
+
+export function FormContact() {
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log("Form submitted");
+  };
+  return (
+    <div className=" w-full mx-auto rounded-none md:rounded-2xl border p-4 md:p-8 shadow-input bg-black">
+      <h2 className="font-bold text-xl text-neutral-200">Contactez-nous</h2>
+      <p className=" text-sm max-w-sm mt-2 text-neutral-300">
+        Login to aceternity if you can because we don&apos;t have a login flow
+        yet
+      </p>
+
+      <form className="mt-8" onSubmit={handleSubmit}>
+        <div className="flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-2 mb-4">
+          <LabelInputContainer>
+            <Label htmlFor="firstname">Prénom</Label>
+            <Input id="firstname" placeholder="Tyler" type="text" />
+          </LabelInputContainer>
+          <LabelInputContainer>
+            <Label htmlFor="lastname">Nom</Label>
+            <Input id="lastname" placeholder="Durden" type="text" />
+          </LabelInputContainer>
+        </div>
+        <LabelInputContainer className="mb-4">
+          <Label htmlFor="email">Email</Label>
+          <Input id="email" placeholder="projectmayhem@fc.com" type="email" />
+        </LabelInputContainer>
+        <LabelInputContainer className="mb-4">
+          <Label htmlFor="tel">Tel</Label>
+          <Input id="tel" placeholder="07 68 73 52 38" type="tel" />
+        </LabelInputContainer>
+        <LabelInputContainer className="mb-4">
+          <Textarea
+            placeholder="Tapez votre texte ici..."
+            name="message"
+            id="message"
+          />
+        </LabelInputContainer>
+
+        <button
+          className="bg-gradient-to-b gap-4 relative group/btn flex items-center justify-center from-zinc-900 to-zinc-900   bg-zinc-800 w-full text-white rounded-md h-10 font-medium shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset]"
+          type="submit"
+        >
+          Envoyer
+          <IconSend stroke={2} width={18} height={18} />
+        </button>
+      </form>
+    </div>
+  );
+}
+
+const BottomGradient = () => {
+  return (
+    <>
+      <span className="group-hover/btn:opacity-100 block transition duration-500 opacity-0 absolute h-px w-full -bottom-px inset-x-0 bg-gradient-to-r from-transparent via-cyan-500 to-transparent" />
+      <span className="group-hover/btn:opacity-100 blur-sm block transition duration-500 opacity-0 absolute h-px w-1/2 mx-auto -bottom-px inset-x-10 bg-gradient-to-r from-transparent via-indigo-500 to-transparent" />
+    </>
+  );
+};
+
+const LabelInputContainer = ({
+  children,
+  className,
+}: {
+  children: React.ReactNode;
+  className?: string;
+}) => {
+  return (
+    <div className={cn("flex flex-col space-y-2 w-full", className)}>
+      {children}
+    </div>
+  );
+};
