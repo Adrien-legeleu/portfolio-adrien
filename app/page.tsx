ee26ede9937c082481ed61342de1d7b1acd57d9b
Test <adrienlegeleu@gmail.com>
2025-01-01 13:05:08 +0100

    bug

--- app/page.tsx
+++ app/page.tsx
@@ -2,14 +2,12 @@ import { About } from "./component/About/About";
 import Approach from "./component/Approach/Approach";
 import Contact from "./component/Contact/Contact";
 import Landing from "./component/Landing/Landing";
-import Project from "./component/Project/Project";
 
 export default function Home() {
   return (
     <div className="lg:px-20 xs:px-10 px-5 overflow-x-hidden">
       <Landing />
       <About />
-      <Project />
       <Approach />
       <Contact />
     </div>
